02200e0b830ac9ab80502106defc2f3e8614a3c5
Rayhanma <67584512+Mr22XX@users.noreply.github.com>
2023-08-27 11:35:26 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+let container = document.querySelector('.container')
+
+function mode(){
+    container.classList.toggle('mode')
+}
+
+function scroll1(){
+    let home = document.querySelector('.content3')
+    home.scrollIntoView()
+}
+
+function scroll2(){
+    let media = document.querySelector('.content4')
+    media.scrollIntoView()
+}
